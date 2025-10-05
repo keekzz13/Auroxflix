dbcb89b3bfe000a1ddd66c8f4da4093ac5530062
Auroxxx <151619153+keekzz13@users.noreply.github.com>
2025-10-05 19:11:31 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import './styles/animations.css'
-import App from './router.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/index.css';
+import './styles/animations.css';
+import App from './router.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
