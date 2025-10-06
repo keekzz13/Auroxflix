4a986ac42fb7e5b872b332fe3304fbd4294ddc12
Auroxxx <151619153+keekzz13@users.noreply.github.com>
2025-10-06 15:26:15 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  css: {
+    postcss: './postcss.config.cjs', // Ensure this points to the correct file
+  },
+});
